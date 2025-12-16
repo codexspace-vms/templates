c1244555307a01a11c2e1f12a969950a8b8a1d83
codexspace-vms <codespacevms@outlook.com>
2025-12-16 13:53:10 +0000

    good git commit -m!
    's

--- cordova/idx-template.nix
+++ cordova/idx-template.nix
@@ -1,37 +1,18 @@
-{ pkgs, packageManager ? "npm", ... }: {
+{ pkgs, appName, appId, ... }: {
   packages = [
     pkgs.nodejs_20
-    pkgs.j2cli
-    pkgs.nixfmt
   ];
   bootstrap = ''
-    mkdir -p "$WS_NAME"
+    mkdir -p "$WS_NAME/. idx"
     
-    # 1. Create the base Cordova project
-    # We use npx to ensure we use a temporary runner without global side effects
-    export npm_config_yes=true
-    npx cordova create "$WS_NAME" com.example.helloworld HelloWorld
+    # Copy the dev.nix configuration
+    cp ${./devNix.j2} "$WS_NAME/.idx/dev.nix"
+    sed -i "s/{{appName}}/${appName}/g" "$WS_NAME/.idx/dev.nix"
+    sed -i "s/{{appId}}/${appId}/g" "$WS_NAME/.idx/dev.nix"
     
-    # 2. Enter project directory
-    cd "$WS_NAME"
+    # Copy README
+    cp ${./README.j2} "$WS_NAME/README.md"
     
-    # 3. Initialize package.json to manage dependencies
-    npm init -y
-    
-    # 4. Install Cordova locally so it is available in the workspace
-    npm install cordova --save-dev
-    
-    # 5. Add the Android platform
-    npx cordova platform add android
-    
-    # 6. Generate configuration files using the selected package manager
-    mkdir -p .idx
-    packageManager=${packageManager} j2 ${./devNix.j2} -o .idx/dev.nix
-    packageManager=${packageManager} j2 ${./README.j2} -o README.md
-    
-    # 7. Final cleanup and move to output
-    cd ..
     chmod -R +w "$WS_NAME"
-    mv "$WS_NAME" "$out"
   '';
 }
